738f6ed99f904e4f0800367cc61c8e7ffb168adb
Akhilks03 <akhilanak03@gmail.com>
2024-10-09 13:06:53 +0530

    Latest Update

--- assets/scss/akhil.scss
+++ assets/scss/akhil.scss
@@ -1,6 +1,6 @@
 /*!
 =========================================================
-* JohnDoe Landing page
+* Akhil Landing page
 =========================================================
 
 * Copyright: 2019 DevCRUD (https://devcrud.com)
